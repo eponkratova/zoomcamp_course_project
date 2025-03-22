7c64d5cc4fb997bcfdf0862ea68e87ea1218b635
eka Ponkratova <kate.ponkratova@gmail.com>
2025-03-22 17:38:43 +0200

    push updated files

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 from fastapi import FastAPI, HTTPException, Query
 import pandas as pd
+import boto3
+import io
+import os
 
 app = FastAPI()
 
-# Load CSV and handle NaN values for JSON compatibility
-df = pd.read_csv("D:/OneDrive/Documents/zoomcamp/transaction_data.csv")
+def load_csv_from_s3():
+    """The function loads CSV from S3"""
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        region_name="us-east-2"
+    )
+    bucket_name = "s3://zoomcamp-transaction/"
+    object_key = "transaction_data.csv"  
+
+    response = s3.get_object(Bucket=bucket_name, Key=object_key)
+    return pd.read_csv(io.BytesIO(response['Body'].read()))
+
+# Load once on startup
+df = load_csv_from_s3()
 df = df.where(pd.notnull(df), None)
 
-@app.get("/transaction_details") #type of the http request
-def get_data( 
+@app.get("/transaction_details")
+def get_data(
     page: int = Query(1, ge=1, description="Page number (starting at 1)"),
-    page_size: int = Query(100, ge=1, le=1000, description="Number of items per page") #num of items from 1 to 1000
+    page_size: int = Query(100, ge=1, le=1000, description="Number of items per page")
 ):
     total = len(df)
     start = (page - 1) * page_size
-    end = start + page_size
-    if start >= total:
-        raise HTTPException(status_code=404, detail="No data found") #loops throughout the pages and throws the error if the data is not found
-    page_data = df.iloc[start:end].to_dict(orient="records") #return .json objects
-    return {
-        "page": page,
-        "page_size": page_size,
-        "total": total,
-        "data": page_data
-    }
+    end = start + page
