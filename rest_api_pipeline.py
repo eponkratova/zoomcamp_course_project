ae58c00506b9434fc4c6d0d6de4083167c3eecb5
eka Ponkratova <kate.ponkratova@gmail.com>
2025-03-23 10:59:53 +0200

    corrected main py

--- rest_api_pipeline.py
+++ rest_api_pipeline.py
@@ -48,7 +48,7 @@ def run_s3_pipeline() -> None:
     pipeline = dlt.pipeline(
         pipeline_name="s3_pipeline",
         destination="filesystem",
-        dataset_name="dl_transaction_details"
+        dataset_name="s3_transaction_details"
     )
     load_info = pipeline.run(custom_source())
     print("Filesystem pipeline load info:", load_info)
